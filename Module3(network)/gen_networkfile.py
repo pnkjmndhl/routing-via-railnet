34cce7088ebb6195b8fe24a1ca521c10178f5180
Pankaj <fusion.pankaj@gmail.com>
2018-10-02 23:24:26 -0400

    removed vlookup and added merge

--- Module3(network)/gen_networkfile.py
+++ Module3(network)/gen_networkfile.py
@@ -11,8 +11,8 @@ arcpy.env.workspace = r"C:/GIS"
 arcpy.env.overwriteOutput = True
 
 # shape files
-network_shp = r'..\GIS\alllinks.shp'
-#network_shp = r'..\GIS\test_alllinks.shp'
+link_shp = r'..\GIS\alllinks.shp'
+# link_shp = r'..\GIS\test_alllinks.shp'
 node_shp = r'..\GIS\allnodes.shp'
 
 # excel files
@@ -21,51 +21,38 @@ exception_xl = r"input\networkexc.xlsx"
 
 # colnames and format and other lists
 integer_list = ['ID', 'A_NODE', 'B_NODE', 'ML_CLASS', 'LINK_TYPE', 'NO_RRS', 'RR1', 'RR2', 'RR3', 'RR4', 'RR5', 'RR6',
-                'RR7',
-                'RR8']
+                'RR7', 'RR8']
 all_list = ['ID', 'A_NODE', 'B_NODE', 'LENGTH', 'TTIME', 'CAPACITY', 'P1', 'P2', 'GAMMA', 'TADJ', 'ML_CLASS',
             'LINK_TYPE', 'NO_RRS', 'RR1', 'RR2', 'RR3', 'RR4', 'RR5', 'RR6', 'RR7', 'RR8']
 format_list = ['{:5d}', '{:5d}', '{:5d}', '{:6.1f}', '{:6.2f}', '{:6.1f}', '{:10.6f}', '{:10.6f}', '{:6.2f}', '{:6.2f}',
                '{:1d}', '{:1d}', '{:1d}', '{:3d}', '{:3d}', '{:3d}', '{:3d}', '{:3d}', '{:3d}', '{:3d}', '{:3d}']
 RRs = ['RR1', 'RR2', 'RR3', 'RR4', 'RR5', 'RR6', 'RR7', 'RR8', 'RR9']
 
-
 # network exception file forces to changes some specific attributes of the network
-# this feature was used to reduce the capacity on line to make the flow through memphis correct.
-def overwrite_exceptions_from_xl(attribute_name):
-    attribute = exceptions_df.parse(attribute_name)
-    for i in range(len(attribute['ID'])):
-        network_df[attribute_name][network_df.ID == attribute['ID'][i]] = attribute[attribute_name][i]
-
 
 # main program
 # read from excel and shp files
-exceptions_df = pandas.ExcelFile(exception_xl)
-to_constants_df = pandas.ExcelFile(to_constants).parse("Sheet2")
-network_df = Dbf5(network_shp.split(".shp")[0] + ".dbf").to_dataframe()
+signal_capacity_to_constants = pandas.ExcelFile(to_constants).parse("Sheet2")
+network_df = Dbf5(link_shp.split(".shp")[0] + ".dbf").to_dataframe()
 
 # calculate columns
+columns_network = list(network_df.columns.values)
+RRs_not_present = [x for x in RRs if x not in columns_network]
+for RR in RRs_not_present:
+    network_df[RR]=0
 network_df['DIR'] = 0
-network_df['RR8'] = 0
-network_df['RR9'] = 0
 network_df['NO_RRS'] = (network_df[RRs] != 0).astype(int).sum(axis=1)
 network_df['TTIME'] = network_df['LENGTH'] / network_df['FF_SPEED'] * 60  # travel time in minutes (FFS)
 network_df['TADJ'] = 0
 
 # COEFFS.XLS has the default values of the columns for all values of SIGNAL and CAPY_CODE
-to_constants_df.columns = [['trackclass', 'SIGNAL', 'CAPY_CODE', 'to', 'P1', 'P2', 'GAMMA', 'CAPACITY', 'miles']]
-to_constants_df['Vlookup'] = to_constants_df['SIGNAL'].astype(str) + to_constants_df['CAPY_CODE'].astype(str)
-network_df['Vlookup'] = network_df['SIGNAL'].astype(str) + network_df['CAPY_CODE'].astype(str)
-network_df = pandas.merge(network_df, to_constants_df, how='left', left_on='Vlookup', right_on='Vlookup')
-
-# overwrite capacity from networkexc.xlsx
-for sheet_names in exceptions_df.sheet_names:
-    overwrite_exceptions_from_xl(sheet_names)
+signal_capacity_to_constants.columns = [['trackclass', 'SIGNAL', 'CAPY_CODE', 'to', 'P1', 'P2', 'GAMMA', 'CAPACITY', 'miles']]
+network_df = pandas.merge(network_df, signal_capacity_to_constants, how='left', left_on=['SIGNAL', 'CAPY_CODE'],
+                          right_on=['SIGNAL', 'CAPY_CODE'])
 
 network_df = network_df[all_list]
 
 # changing linkIDS
-# network.ID = range(1000, len(network)+1000)
 network_df.to_csv(r"intermediate\network.csv")
 
 dictionary_of_column_name_and_format = dict(zip(all_list, format_list))
--- Module4(transfers)/runandcopy.bat
+++
@@ -1,6 +0,0 @@
-python 1gen_transfershp_from_xl.py
-if errorlevel 1 pause
-python 2snap_plot_gen_transferfile.py
-if errorlevel 1 pause
-copy output/network.xfr C:\Users\pankaj\Desktop\RAIL\Netdata\
-if errorlevel 1 pause
