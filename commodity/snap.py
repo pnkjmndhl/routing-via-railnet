9976e238b2cc8cb4e0d15df2ed5a08db69e0e8ec
Pankaj <fusion.pankaj@gmail.com>
2018-10-18 12:12:29 -0400

    Added feature: manual snaps by commodity by railroad by fips

--- commodity/snap.py
+++ commodity/snap.py
@@ -1,53 +1,8 @@
 from rail import *
 import arcpy
-
-OD = pandas.read_csv(r"intermediate/" + sys.argv[1] + ".csv")
-# if the startRR is not in our network, add 0 (can snap to any railroad)
-network_railroad_list = get_network_rrs()
-OD['startRR'][~OD.startRR.isin(network_railroad_list)] = 0
-
-fips_nearnodeid_dictionary = {}
-
-
-def update_nearest_node_dictionary():
-    arcpy.Near_analysis(fips_shp, node_shp, "", "NO_LOCATION", "NO_ANGLE", "GEODESIC")
-    fips_to_near_fid = {row.getValue("FIPS"): row.getValue("NEAR_FID") for row in
-                        arcpy.SearchCursor(fips_shp)}
-    near_fid_to_node_id = {row.getValue("FID"): row.getValue("ID") for row in
-                           arcpy.SearchCursor(node_shp)}
-    fips_nearnodeid_dictionary = {x: near_fid_to_node_id[y] for x, y in fips_to_near_fid.iteritems()}
-    return fips_nearnodeid_dictionary
-
-
-fips_nearnodeid_dictionary = update_nearest_node_dictionary()
-
-# arguments
-if sys.argv[1] in ['-h', 'help']:
-    print("Usage: python snap.py filename update/new ")
-
-if sys.argv[2] == "new":
-    # read the new copy of csv file
-    index_of_od = range(len(OD))
-    fips_orr_to_node_odist_df = pandas.DataFrame({"OFIPS": [], "startRR": [], "ONODE": [], "ODIST": []})
-
-elif sys.argv[2] == "update":
-    # read the new copy of csv file
-    # vector calculation
-    fips_orr_to_node_odist_df = pandas.read_csv(r"intermediate\OFIPSORR.csv")
-    # empty_base_df = pandas.merge(OD, fips_orr_to_node_odist_df, left_on=['OFIPS', 'startRR'], right_on=['OFIPS', 'startRR'], how='left')
-    OD['ONode'] = pandas.merge(OD, fips_orr_to_node_odist_df, how='left', on=['OFIPS', 'startRR'])['ONODE']
-    OD['ODIST'] = pandas.merge(OD, fips_orr_to_node_odist_df, how='left', on=['OFIPS', 'startRR'])['ODIST_y']
-    OD['DNode'] = OD.DFIPS.map(fips_nearnodeid_dictionary)
-    index_of_od = OD.index[OD.ONode.isnull()]
-
-else:
-    print("Invalid arguments")
-    exit(0)
-
-# get the name of columns
-column_list = list(OD.columns.values)
-column_list = [x for x in column_list if 'Unnamed' not in x]
-OD = OD[column_list]
+import re
+import math
+import numpy as np
 
 # change the encoding to utf-8
 reload(sys)
@@ -55,10 +10,29 @@ sys.setdefaultencoding('utf-8')
 
 # overwrite files if its already present
 arcpy.env.overwriteOutput = True
-# arcpy.env.autoCancelling = False
 
+if sys.argv[2] not in ['new', 'update', '-h']:
+    print sys.argv[1]
+    print("Invalid Arguments")
+    exit(0)
+elif sys.argv[2] in ['-h', '-help']:
+    print("Usage: python snap.py filename update/new ")
+    exit(0)
 
-#################work on copies####################
+OD = pandas.read_csv(r"intermediate/" + sys.argv[1] + ".csv")
+
+# if the startRR is not in our network, add 0 (can snap to any railroad)
+network_railroad_list = get_network_rrs()
+OD['startRR'][~OD.startRR.isin(network_railroad_list)] = 0
+
+fips_nearnodeid_dictionary = {}
+
+# get the name of columns
+column_list = list(OD.columns.values)
+column_list = [x for x in column_list if 'Unnamed' not in x]
+OD = OD[column_list]
+
+#################  work on copies  ####################
 # fips_shp1 = "C:/GIS/FIPS.shp"
 # node_shp1 = "C:/GIS/allnodes.shp"
 # link_shp1 = "C:/GIS/alllink.shp"
@@ -69,31 +43,35 @@ arcpy.env.overwriteOutput = True
 # node_shp = node_shp1
 # link_shp = link_shp1
 
+
+fips_orr_comm_to_node_odist_df = pandas.read_csv(r"intermediate\OFIPSORRcomm.csv")
+
 arcpy.MakeFeatureLayer_management(link_shp, link_shpf)
 arcpy.MakeFeatureLayer_management(fips_shp, fips_shpf)
 arcpy.MakeFeatureLayer_management(node_shp, node_shpf)
 
-
-# def get_where_clause(rr):
-#     where_clause = ""
-#     whereclauselist = ["\""+rrs+"\"= " + str(rr) ]
-#     for rrs in railroads_list:
-#         where_clause = where_clause + "\""+rrs+"\"= " + str(rr)
-#     where_clause = "\"RR1\" = " + str(origin_rr) + " OR \"RR2\" = " + str(origin_rr) + " OR \"RR3\" = " + str(
-#         origin_rr) + " OR \"RR4\" = " + str(origin_rr) + " OR \"RR5\" = " + str(origin_rr) + " OR \"RR6\" = " + str(
-#         origin_rr) + " OR \"RR7\" = " + str(origin_rr)
+all_field_names = [x.name for x in arcpy.ListFields(link_shp)]
+rr_field_names = [x for x in all_field_names if re.match(r'RR\d', x)]
 
 
+def get_where_clause(rr):
+    where_clause = ""
+    if rr == 0:
+        return ""
+    for rrs in rr_field_names:
+        where_clause = where_clause + "\"" + rrs + "\"= " + str(rr) + " OR "
+    where_clause = where_clause[:-4]
+    return where_clause
 
 
-
-
-
-
-
-
-
-
+def update_nearest_node_dictionary():
+    arcpy.Near_analysis(fips_shp, node_shp, "", "NO_LOCATION", "NO_ANGLE", "GEODESIC")
+    fips_to_near_fid = {row.getValue("FIPS"): row.getValue("NEAR_FID") for row in
+                        arcpy.SearchCursor(fips_shp)}
+    near_fid_to_node_id = {row.getValue("FID"): row.getValue("ID") for row in
+                           arcpy.SearchCursor(node_shp)}
+    fips_nearnodeid_dictionary = {x: near_fid_to_node_id[y] for x, y in fips_to_near_fid.iteritems()}
+    return fips_nearnodeid_dictionary
 
 
 # origin_fips = 40063
@@ -119,13 +97,7 @@ def get_nearest_onode_with_orr(origin_fips, origin_rr):
     yes_or_no = get_if_available(origin_fips, origin_rr)
     if yes_or_no != 0:
         return yes_or_no
-    if origin_rr == 0:  # if the originRR == nan, then no need to do the selection based on the railroad
-        where_clause = ""
-    else:
-        where_clause = "\"RR1\" = " + str(origin_rr) + " OR \"RR2\" = " + str(origin_rr) + " OR \"RR3\" = " + str(
-            origin_rr) + " OR \"RR4\" = " + str(origin_rr) + " OR \"RR5\" = " + str(origin_rr) + " OR \"RR6\" = " + str(
-            origin_rr) + " OR \"RR7\" = " + str(origin_rr)
-    arcpy.SelectLayerByAttribute_management(link_shpf, "NEW_SELECTION", where_clause)
+    arcpy.SelectLayerByAttribute_management(link_shpf, "NEW_SELECTION", get_where_clause(origin_rr))
     arcpy.FeatureVerticesToPoints_management(link_shpf, "in_memory/p1", "BOTH_ENDS")
     arcpy.MakeFeatureLayer_management("in_memory/p1", "p1")
     arcpy.SelectLayerByAttribute_management(fips_shpf, "NEW_SELECTION",
@@ -148,18 +120,76 @@ def get_nearest_node(fips):
     return fips_nearnodeid_dictionary[fips]
 
 
-OD['ODIST'] = 0.0
+def add_string(x, str_):
+    if math.isnan(x):
+        return np.nan
+    else:
+        return str_
 
 
+def snap_by_commodity_rr():
+    global OD
+    print("Manual Snapping By Railroad and Commodity.")
+    to_node_odist_df = fips_orr_comm_to_node_odist_df[['FIPS', 'startRR', 'comm', 'NODE']]
+    to_node_odist_df.columns = [['OFIPS', 'startRR', 'comm', 'ONode']]
+    # for both commodity and railroad given
+    OD = OD.reset_index()
+    OD = OD.set_index(['OFIPS', 'startRR', 'comm'])
+    OD.update(to_node_odist_df.set_index(['OFIPS', 'startRR', 'comm'])['ONode'])
+    # for comm = 0 and startRR = 0
+    OD = OD.reset_index()
+    OD = OD.set_index(['OFIPS'])
+    to_node_odist_df_r0_c0 = to_node_odist_df[(to_node_odist_df.comm == 0) & (to_node_odist_df.startRR == 0)]
+    OD.update(to_node_odist_df_r0_c0.set_index(['OFIPS'])['ONode'])
+    # for comm = 0
+    OD = OD.reset_index()
+    OD = OD.set_index(['OFIPS','startRR'])
+    to_node_odist_df_c0 = to_node_odist_df[(to_node_odist_df.comm == 0)]
+    OD.update(to_node_odist_df_c0.set_index(['OFIPS','startRR'])['ONode'])
+    # for startRR = 0
+    OD = OD.reset_index()
+    OD = OD.set_index(['OFIPS','comm'])
+    to_node_odist_df_r0 = to_node_odist_df[to_node_odist_df.startRR == 0]
+    OD.update(to_node_odist_df_r0.set_index(['OFIPS','comm'])['ONode'])
+    OD = OD.reset_index()
+    print("Completed. :)")
 
+
+def snap_by_rr():
+    global fips_orr_to_node_odist_df
+    fips_orr_to_node_odist_df = pandas.read_csv(r"intermediate\OFIPSORR.csv")
+    OD['ONode'] = pandas.merge(OD, fips_orr_to_node_odist_df, how='left', on=['OFIPS', 'startRR'])['ONODE']
+    OD['ODIST'] = pandas.merge(OD, fips_orr_to_node_odist_df, how='left', on=['OFIPS', 'startRR'])['ODIST_y']
+    OD['DNode'] = OD.DFIPS.map(fips_nearnodeid_dictionary)
+
+
+# main program starts here
+# calculate D-node here
+
+# OD['ODIST'] = ""
+fips_nearnodeid_dictionary = update_nearest_node_dictionary()
+
+if sys.argv[2] == "new":
+    # read the new copy of csv file
+    index_of_od = range(len(OD))
+    fips_orr_to_node_odist_df = pandas.DataFrame({"OFIPS": [], "startRR": [], "ONODE": [], "ODIST": []})
+
+elif sys.argv[2] == "update":
+    # read the FIPS to Node conversion csv file
+    snap_by_rr()
+    snap_by_commodity_rr()
+    index_of_od = OD.index[OD.ONode.isnull()]
+
+#  work only for ONode here ...
 for i in index_of_od:
     OD['ONode'][i], OD['ODIST'][i] = get_nearest_onode_with_orr(OD.at[i, 'OFIPS'], OD.at[i, 'startRR'])
-    OD['DNode'][i] = get_nearest_node(OD.at[i, 'DFIPS'])
+
     print ("{0}:    OFP:{1:6.0f}    sRR:{3:3.0f}    OND:{4:6.0f}    DIST:{5:6.2f}".format(i, OD.at[i, 'OFIPS'],
                                                                                           OD.at[i, 'DFIPS'],
                                                                                           OD.at[i, 'startRR'],
                                                                                           OD['ONode'][i],
                                                                                           OD['ODIST'][i]))
+
 fips_orr_to_node_odist_df = fips_orr_to_node_odist_df.drop_duplicates()
 fips_orr_to_node_odist_df[['OFIPS', 'startRR', 'ONODE', 'ODIST']].to_csv(r"intermediate\OFIPSORR.csv")
 OD[column_list].to_csv(r"intermediate/" + sys.argv[1] + "_1.csv")
