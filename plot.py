5b28f2686d19b0b5125a19c678b7281d3d8e358e
Pankaj <fusion.pankaj@gmail.com>
2018-11-09 10:43:26 -0500

    added folder plot, plots all the possible plots in a given folder and no scenario if no any base argument given

--- plot.py
+++ plot.py
@@ -1,4 +1,4 @@
-# plots the LMF file to jpg
+# plots everything in the LMF folder to plot folder
 
 import pandas as pd
 import arcpy
@@ -7,44 +7,21 @@ import os
 
 # default values
 plot_folder = 'Plots/'
-folder_in_LMF = ""  # finds the LMFs in Netdata
 
 # This program plots the output commodity.lmf file to plot.shp
-switch = sys.argv[1]
 
-if switch == '-d':
-    base = sys.argv[2]
-    scenarios = sys.argv[3:]
-    plot_type = "Plotdiff"
+folder_in_LMF = sys.argv[1]
 
-elif switch == '-p':
-    bases = sys.argv[2:]
-    plot_type = "Plot"
+try:
+    base_arg = sys.argv[2]
+except:
+    base_arg = ""
 
-elif switch in ['-df', '-fd']:
-    folder_in_LMF = sys.argv[2]
-    base = sys.argv[3]
-    # all files in the folder except base are scenarios
-    lmfs = os.listdir("./netdata/LMF/" + folder_in_LMF)
-    lmfs = [x.split('.')[0] for x in lmfs if base not in x]
-    scenarios = lmfs
-    plot_folder = 'Plots/' + folder_in_LMF
+plot_folder = 'Plots/' + folder_in_LMF
 
-elif switch in ['-pf', '-fp']:
-    folder_in_LMF = sys.argv[2]
-    lmfs = os.listdir("./netdata/LMF/" + folder_in_LMF)
-    lmfs = [x.split('.')[0] for x in lmfs]
-    # print lmfs
-    # all the lmf files in the folder are bases
-    bases = lmfs
-    plot_folder = 'Plots/' + folder_in_LMF
-
-else:
-    print ("Invalid arguments")
-    print ("Usage: python plot.py [-d|-p] [list of LMFs]")
-    print ("Usage: python plot.py [-df|-fd] [foldername] [basename] [list of LMFs]")
-    print ("Usage: python plot.py [-pf|-fp] [foldername] [list of LMFs]")
-    exit(0)
+bases = os.listdir("./netdata/LMF/" + folder_in_LMF)
+scenarios = [x for x in bases if base_arg not in x]
+base_lmf = [x for x in bases if base_arg in x]
 
 arcpy.env.overwriteOutput = True  # overwrite files if already present
 
@@ -98,7 +75,7 @@ def export_to_jpeg(plot_type, exclude_plot, plot_path):
     now = datetime.datetime.now()
     mxd = arcpy.mapping.MapDocument(r"Rail3_plot.mxd")
     df = arcpy.mapping.ListDataFrames(mxd)[0]
-    print arcpy.mapping.ListLayers(mxd, "", df)[0].name
+    # print arcpy.mapping.ListLayers(mxd, "", df)[0].name
     for lyr in arcpy.mapping.ListLayers(mxd, "", df):
         if lyr.name in plot_type:
             lyr.visible = True
@@ -116,44 +93,47 @@ link_shp = 'gis/alllinks.shp'
 field_names = get_field_names(link_shp)  # getting field names
 field_names = [e for e in field_names if e not in ["ID", "FID", "Shape"]]
 
-if switch in ['-d', '-df', '-fd']:  # scenario plots
-    print("Generating Difference plot ...")
-    plot = 'gis/plots/Plotdiff.shp'
-    file_1 = "Netdata/LMF/" + folder_in_LMF + "/" + base
-    file_1 = file_1.replace("//", "/")  # if folder_in_LMF is empty, the // will be replaced by /
-    for scenario in scenarios:
-        print ("working on: " + base + "," + scenario)
-        file_2 = "Netdata/LMF/" + folder_in_LMF + "/" + scenario
-        file_2 = file_2.replace("//", "/")
-        flow_1 = split_to_dataframe(file_1 + ".LMF", ['ID', 'qty'])
-        flow_2 = split_to_dataframe(file_2 + ".LMF", ['ID', 'qty'])
-        flow = flow_1  # flow1 is always the base flow
-        get_plot_with_fieldnames(field_names)
-        flow['qty'] = flow_2['qty'] - flow_1['qty']
-        add_column_to_shp("qty")
-        plot_type = ["Plotdiff", 'allnodes']
-        exclude_plot = ['Plot']
-        plot_name = base + "_" + scenario
-        plot_path = 'plots/' + folder_in_LMF + "/" + plot_name
-        if folder_in_LMF != "":
-            os.system("Mkdir plots\\" + folder_in_LMF)
-        print plot_path
-        export_to_jpeg(plot_type, exclude_plot, plot_path)
+print("Generating normal plot ...")
+plot = 'gis/plots/Plot.shp'
 
-if switch in ['-f', '-pf', '-fp']:  # base plots
-    print("Generating normal plot ...")
-    plot = 'gis/plots/Plot.shp'
-    for base in bases:
-        print ("working on: " + base)
-        file_name = "Netdata/LMF/" + folder_in_LMF + "/" + base
-        flow = split_to_dataframe(file_name + ".LMF", ['ID', 'qty'])
-        get_plot_with_fieldnames(field_names)
-        add_column_to_shp("qty")
-        plot_type = ['Plot', 'allnodes']
-        exclude_plot = ['Plotdiff']
-        plot_name = base
-        plot_path = 'plots/' + folder_in_LMF + "/" + plot_name
-        if folder_in_LMF != "":
-            os.system("Mkdir plots\\" + folder_in_LMF)
-        print plot_path
-        export_to_jpeg(plot_type, exclude_plot, plot_path)
+for base in bases:
+    print ("working on: " + base)
+    file_name = "Netdata/LMF/" + folder_in_LMF + "/" + base
+    flow = split_to_dataframe(file_name, ['ID', 'qty'])
+    get_plot_with_fieldnames(field_names)
+    add_column_to_shp("qty")
+    plot_type = ['Plot', 'allnodes']
+    exclude_plot = ['Plotdiff']
+    plot_name = base
+    plot_path = 'plots/' + folder_in_LMF + "/" + plot_name
+    if not os.path.exists('plots/' + folder_in_LMF):
+        os.makedirs('plots/' + folder_in_LMF)
+    print plot_path
+    export_to_jpeg(plot_type, exclude_plot, plot_path)
+
+
+if base_arg == "":
+    print("No Scenario Plots generated")
+    exit(0)
+
+print("Generating Difference plot ...")
+plot = 'gis/plots/Plotdiff.shp'
+file_1 = "Netdata/LMF/" + folder_in_LMF + "/" + base_lmf[0]
+
+for scenario in scenarios:
+    print ("working on: " + base + "," + scenario)
+    file_2 = "Netdata/LMF/" + folder_in_LMF + "/" + scenario
+    flow_1 = split_to_dataframe(file_1, ['ID', 'qty'])
+    flow_2 = split_to_dataframe(file_2, ['ID', 'qty'])
+    flow = flow_1  # flow1 is always the base flow
+    get_plot_with_fieldnames(field_names)
+    flow['qty'] = flow_2['qty'] - flow_1['qty']
+    add_column_to_shp("qty")
+    plot_type = ["Plotdiff", 'allnodes']
+    exclude_plot = ['Plot']
+    plot_name = base + "_" + scenario
+    plot_path = 'plots/' + folder_in_LMF + "/" + plot_name
+    if not os.path.exists('plots/' + folder_in_LMF):
+        os.makedirs('plots/' + folder_in_LMF)
+    print plot_path
+    export_to_jpeg(plot_type, exclude_plot, plot_path)
