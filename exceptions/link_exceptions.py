1b6ca972273ff80fed27d2bff5a45bb70467aefa
Pankaj <fusion.pankaj@gmail.com>
2018-11-19 11:07:07 -0500

    now creates a buffer of 100 feet around the link_exception links to find out the IDs of the links with that attributes

--- exceptions/link_exceptions.py
+++ exceptions/link_exceptions.py
@@ -1,42 +1,53 @@
 from rail import *
 import arcpy
 
+temp_layer = "C:/GIS/temp.shp"
+link_exception_shpf = "link_exception_dfeptionshpf"
 arcpy.env.overwriteOutput = True
 
-arcpy.Near_analysis(link_exception_shp, link_shp, "", "NO_LOCATION", "NO_ANGLE", "GEODESIC")
 
-link_exception_shp_dict = {
-row.getValue("FID"): [row.getValue("dir"), row.getValue("RR") row.getValue("comm"), row.getValue("costptrhr"),
-                      row.getValue("costptml"), row.getValue("NEAR_FID")] for row in
-arcpy.SearchCursor(link_exception_shp)}
+arcpy.MakeFeatureLayer_management(link_shp, link_shpf)
+arcpy.MakeFeatureLayer_management(link_exception_shp, link_exception_shpf)
 
-link_fid_id_dict = {row.getValue("FID"): row.getValue("ID") for row in arcpy.SearchCursor(link_shp}
 
-for key, value in link_exception_shp_dict.items():
-    value[4] = link_fid_id_dict[value[4]]
+link_exception_df = pandas.DataFrame({"ID":[],"DIR":[], "RR":[], "COSTPTRHR":[],"COSTPTML":[]})
+link_exception_dict = {}
 
-transfer_exception_df = pandas.DataFrame(link_exception_shp_dict).transpose()
-transfer_exception_df.columns = [["DIR", "RR", "comm", "costpthr", "costptml", "ID"]]
+def get_field_names(shp):
+    fieldnames = [f.name for f in arcpy.ListFields(shp)]
+    return fieldnames
 
-transfer_exception_df = transfer_exception_df[['DIR', 'ID', 'RR', 'costpthr', 'costptml']]
 
-# 2 conflicting... need to figure out the right way
+with arcpy.da.SearchCursor(link_exception_shp, ["FID","dir", "RR", "comm", "costptrhr", "costptml"]) as cursor:
+    for row in cursor:
+        where_clause = """ "FID" = %d""" % row[0]
+        arcpy.SelectLayerByAttribute_management(link_exception_shpf, "NEW_SELECTION", where_clause)
+        arcpy.Buffer_analysis(link_exception_shpf, temp_layer, "100 feet")
+        arcpy.SelectLayerByLocation_management(link_shpf, "COMPLETELY_WITHIN", temp_layer)
+        IDs = [row1.getValue("ID") for row1 in arcpy.SearchCursor(link_shpf)]
+        for ID in IDs:
+            link_exception_dict[ID] = [row[1], row[2], row[3], row[4], row[5]]
 
-linkexc = linkexc[['ID', 'RR', 'comm', 'direction', 'costpertrainhr', 'costpertonmile']]
-linkexc.ID = linkexc.ID.astype(int)
-linkexc.RR = linkexc.RR.astype(int)
-linkexc.comm = linkexc.comm.astype(int)
-linkexc.direction = linkexc.direction.astype(int)
-linkexc.costpertonmile = linkexc.costpertonmile.astype(float)
 
-linkexc['ID'] = linkexc['ID'].map('{:5d}'.format)
-linkexc['RR'] = linkexc['RR'].map('{:5d}'.format)
-linkexc['comm'] = linkexc['comm'].map('{:5d}'.format)
-linkexc['direction'] = linkexc['direction'].map('{:5d}'.format)
-linkexc['costpertrainhr'] = linkexc['costpertrainhr'].map('{:10.2f}'.format)
-linkexc['costpertonmile'] = linkexc['costpertonmile'].map('{:10.2f}'.format)
+link_exception_df = pandas.DataFrame(link_exception_dict).transpose().reset_index()
+link_exception_df.columns = ["ID", "direction", "RR", "comm", "costpertrainhr", "costpertonmile"]
 
-linkexc = linkexc[['ID', 'RR', 'comm', 'direction', 'costpertrainhr', 'costpertonmile']].apply(
+
+link_exception_df = link_exception_df[['ID', 'RR', 'comm', 'direction', 'costpertrainhr', 'costpertonmile']]
+link_exception_df.ID = link_exception_df.ID.astype(int)
+link_exception_df.RR = link_exception_df.RR.astype(int)
+link_exception_df.comm = link_exception_df.comm.astype(int)
+link_exception_df.direction = link_exception_df.direction.astype(int)
+link_exception_df.costpertonmile = link_exception_df.costpertonmile.astype(float)
+
+link_exception_df['ID'] = link_exception_df['ID'].map('{:5d}'.format)
+link_exception_df['RR'] = link_exception_df['RR'].map('{:5d}'.format)
+link_exception_df['comm'] = link_exception_df['comm'].map('{:5d}'.format)
+link_exception_df['direction'] = link_exception_df['direction'].map('{:5d}'.format)
+link_exception_df['costpertrainhr'] = link_exception_df['costpertrainhr'].map('{:10.5f}'.format)
+link_exception_df['costpertonmile'] = link_exception_df['costpertonmile'].map('{:10.5f}'.format)
+
+link_exception_df = link_exception_df[['ID', 'RR', 'comm', 'direction', 'costpertrainhr', 'costpertonmile']].apply(
     lambda x: '{}{}{}{}{}{}'.format(x[0], x[1], x[2], x[3], x[4], x[5]), axis=1)
 
-linkexc.to_csv("link.exc", index=False)
+link_exception_df.to_csv("output/link.exc", index=False)
