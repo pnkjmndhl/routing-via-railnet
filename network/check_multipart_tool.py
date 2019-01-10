d2e5530d039bfc2c597ee608b1cd78acefcd0b43
Pankaj <fusion.pankaj@gmail.com>
2019-01-10 12:23:11 -0500

    a tool that finds out the links with multipart features

--- network/check_multipart_tool.py
+++ network/check_multipart_tool.py
@@ -1,10 +1,11 @@
+from rail import *
 import arcpy
 
 multiparts = []
 
-for row in arcpy.da.SearchCursor(link_shp, ["OID@", 'ID' "SHAPE@"]):
+for row in arcpy.da.SearchCursor(link_shp, ["OID@", 'ID', "SHAPE@"]):
     if row[2].isMultipart is True:
-        multiparts.append(row[0])
+        multiparts.append(row[1])
 
 print("List of IDs with multipart: ")
 print(multiparts)
