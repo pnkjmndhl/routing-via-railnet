272e9abcec566e855091f3e54106a92f65310d27
Pankaj Dahal <fusion.pankaj@gmail.com>
2019-12-18 10:05:52 -0500

    create new nodes at every run
    run this file to recreate the nodeIDs, all previous nodeids would be lost and the commodity file has to be run again

--- network/generate_nodeids.py
+++ network/generate_nodeids.py
@@ -24,10 +24,8 @@ arcpy.SpatialJoin_analysis(node_shp, state_shp, temp, "JOIN_ONE_TO_ONE", "KEEP_A
 
 #get all the field names and drop them (since they are imported from link_shp)
 field_names =  [f.name for f in arcpy.ListFields(temp)]  # getting field names
-try:
-    arcpy.AddField_management(temp, "ID", "LONG")
-except: #if already has IDs, all should be 0 at first (later calculated)
-    arcpy.CalculateField_management(temp, "ID", 0, "PYTHON")  # all 0 at first
+arcpy.AddField_management(temp, "ID", "LONG")
+arcpy.CalculateField_management(temp, "ID", 0, "PYTHON")  # all 0 at first
 field_names = [e for e in field_names if e not in ["ID", "FID", "Shape", "STATE_FP"]]
 arcpy.DeleteField_management(temp, field_names)
 
@@ -35,12 +33,9 @@ arcpy.DeleteField_management(temp, field_names)
 #all states contain no nodes at first
 count_of_county_dict = {x: 0 for x in range(1, 99)}
 
-
 print("List of Nodes added:")
 with arcpy.da.UpdateCursor(temp, ["ID", "STATE_FP"]) as cursor:
     for row in cursor:
-        if row[0] != 0:
-            continue
         row[0] = 1000 * row[1] + count_of_county_dict.get(row[1]) + 1
         count_of_county_dict.update({row[1]: count_of_county_dict.get(row[1]) + 1})
         cursor.updateRow(row)
